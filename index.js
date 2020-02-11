3243788e9fb9d9a6ab709ddf4212abd3513bb843
sooshe <sooshe@sooshebot.com>
2020-02-11 13:35:43 -0600

    move hubs folder out of data

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const dbRouter = require("./hubs/db-router");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/api/hubs", dbRouter);
+
+server.get("/", (req, res) => {
+  res.send(`
+    <h1>Is this working? Cool</h1>
+  `);
+});
+
+// add an endpoint that returns all the messages for a hub
+// add an endpoint for adding new message to a hub
+const port = 7000;
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
