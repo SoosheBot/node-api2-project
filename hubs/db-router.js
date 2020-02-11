4794e76bb634e662aeefb9996e1c49da439b3324
sooshe <sooshe@sooshebot.com>
2020-02-11 15:43:57 -0600

    get all success

--- hubs/db-router.js
+++ hubs/db-router.js
@@ -4,4 +4,15 @@ const Hubs = require("./db");
 
 const router = express.Router();
 
+router.get('/', (req,res) => {
+    Hubs.find(req.query)
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(err => {
+        console.log('find error', err);
+        res.status(500).json({ errorMessage: "could not find posts"});
+    });
+});
+
 module.exports = router;
